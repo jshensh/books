884dbaaa1b81d51b2fbb6455ea832169d122a9e2
jshensh <admin@imjs.work>
2020-09-05 01:40:36 +0800

    收尾工作

--- config/system.php
+++ config/system.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'admin_pass'    => 'admin', // 管理员密码
-    'loanshare_key' => 'flk;sdjfksdfjksdjfksdfj' // 加密 Key
+    'admin_pass'    => '', // 管理员密码
+    'loanshare_key' => '' // 加密 Key
 ];
